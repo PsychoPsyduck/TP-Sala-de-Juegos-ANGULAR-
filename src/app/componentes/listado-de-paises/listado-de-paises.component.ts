68dda8e981f0b9b77202a200d2775e32523b446c
Nicolas.Sande <nialsande@gmail.com>
2020-09-04 17:08:53 -0300

    actualizo angular y package.json

--- src/app/componentes/listado-de-paises/listado-de-paises.component.ts
+++ src/app/componentes/listado-de-paises/listado-de-paises.component.ts
@@ -14,11 +14,11 @@ export class ListadoDePaisesComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.miServicioDePaises.listar()
-    .then(datos=>{
-      console.info("listado de paises",datos);
-      this.listadoDePaises=datos;
-    });
+    // this.miServicioDePaises.listar()
+    // .then(datos=>{
+    //   console.info("listado de paises",datos);
+    //   this.listadoDePaises=datos;
+    // });
   }
 
 }
--- src/app/servicios/mi-http.service.ts
+++ src/app/servicios/mi-http.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
 
-import {Http ,Response} from '@angular/http';
+// import {Http ,Response} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
-import {Observable} from 'rxjs/Observable';
+// import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class MiHttpService {
   
-  constructor(public http:Http) { }
+  constructor(){}//public http:Http) { }
   
-  public httpGetPromise(url: string, objeto:any){
+  // public httpGetPromise(url: string, objeto:any){
 
 
-    return this.http
-    .get(url)
-    .toPromise()
-    .then(this.extraerDatos)
-    .catch(this.handleError);
-  }
+  //   return this.http
+  //   .get(url)
+  //   .toPromise()
+  //   .then(this.extraerDatos)
+  //   .catch(this.handleError);
+  // }
 
   private extraerDatos(resp:Response) {
 
