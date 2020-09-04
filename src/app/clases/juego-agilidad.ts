6e4e68915167890fbd33c25301bd9d31ac7dfed2
Nicolas.Sande <nialsande@gmail.com>
2020-09-04 18:41:05 -0300

    agrego clase agilidad aritmetica

--- src/app/clases/juego-agilidad.ts
+++ src/app/clases/juego-agilidad.ts
@@ -1,2 +1,13 @@
-export class JuegoAgilidad {
+import { Juego } from '../clases/juego'
+
+export class JuegoAgilidad extends Juego {
+    public primerNumero;
+    public operador;
+    public segundoNumero;
+    public numeroIngresado: number;
+    public gano;
+
+    public verificar() {
+        return false;
+    }
 }
